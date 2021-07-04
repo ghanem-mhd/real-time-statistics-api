1e8142b49465e27446083b0af92a16868b7d6757
Mohammad Ghanem <ghanem.mhd95@gmail.com>
2021-07-04 14:00:20 +0200

    Add transactions service

---
+++ src/main/java/com/example/realtimestatisticsapi/transactions/TransactionsService.java
@@ -0,0 +1,31 @@
+package com.example.realtimestatisticsapi.transactions;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Collection;
+
+@Service
+public class TransactionsService {
+
+    private static final int TRANSACTION_AGE_IN_SECONDS = 60;
+
+    @Autowired
+    TransactionsRepository repository;
+
+    public Transaction saveTransaction(Transaction transaction){
+        return repository.save(transaction);
+    }
+
+    public Collection<Transaction> getAllTransaction() {
+        return repository.getAll();
+    }
+
+    public boolean isTransactionOld(Transaction transaction){
+        if (transaction == null || transaction.getTimestamp() == null ){
+            return false;
+        }
+        return Instant.now().minusSeconds(TRANSACTION_AGE_IN_SECONDS).isAfter(transaction.getTimestamp().toInstant());
+    }
+}
---
+++ src/test/java/com/example/realtimestatisticsapi/transactions/TransactionsServiceTests.java
@@ -0,0 +1,48 @@
+package com.example.realtimestatisticsapi.transactions;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TransactionsServiceTests {
+
+    private TransactionsService service;
+
+    @BeforeEach
+    public void initEach() {
+        service = new TransactionsService();
+    }
+
+    @Test
+    public void isTransactionOld_whenCalledForNullTransaction_shouldReturnFalse(){
+        assertFalse(service.isTransactionOld(null));
+    }
+
+    @Test
+    public void isTransactionOld_whenCalledForTransactionWithNullTimestamp_shouldReturnFalse(){
+        assertFalse(service.isTransactionOld(new Transaction(null, null)));
+    }
+
+    @Test
+    public void isTransactionOld_whenCalledForTransactionWithTimestampInLast60Seconds_shouldReturnFalse(){
+        Transaction transaction = new Transaction(new Date(), BigDecimal.ZERO);
+        assertFalse(service.isTransactionOld(transaction));
+    }
+
+    @Test
+    public void isTransactionOld_whenCalledForTransactionWithTimestampOlderThan60Seconds_shouldReturnTrue(){
+        Transaction transaction = new Transaction(new Date(System.currentTimeMillis() - 1000 * 60), BigDecimal.ZERO);
+        assertTrue(service.isTransactionOld(transaction));
+    }
+
+    @AfterEach
+    public void tearDownEach(){
+        service = null;
+    }
+}
