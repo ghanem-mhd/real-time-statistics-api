69a8a61af456923329171c6e92864fbb064f98ec
Mohammad Ghanem <ghanem.mhd95@gmail.com>
2021-07-04 12:51:43 +0200

    Add hibernate-validator package

--- src/main/java/com/example/realtimestatisticsapi/RealTimeStatisticsApiApplication.java
+++ src/main/java/com/example/realtimestatisticsapi/RealTimeStatisticsApiApplication.java
@@ -2,22 +2,13 @@ package com.example.realtimestatisticsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class RealTimeStatisticsApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RealTimeStatisticsApiApplication.class, args);
     }
 
-}
-
-@RestController
-class HelloWorldController {
-    @RequestMapping("/")
-    public String hello() {
-        return "Hello World!";
-    }
 }
