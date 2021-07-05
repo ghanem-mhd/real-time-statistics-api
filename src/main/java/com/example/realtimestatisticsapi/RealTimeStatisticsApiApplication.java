3eb320921826d2422f243df07b1f8735294938d6
Mohammad Ghanem <ghanem.mhd95@gmail.com>
2021-07-05 02:07:22 +0200

    WIP

--- src/main/java/com/example/realtimestatisticsapi/RealTimeStatisticsApiApplication.java
+++ src/main/java/com/example/realtimestatisticsapi/RealTimeStatisticsApiApplication.java
@@ -3,7 +3,11 @@ package com.example.realtimestatisticsapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableScheduling
@@ -13,4 +17,13 @@ public class RealTimeStatisticsApiApplication {
         SpringApplication.run(RealTimeStatisticsApiApplication.class, args);
     }
 
+}
+
+@RestController
+class IndexController {
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping("/")
+    public String index() {
+        return "Ok!";
+    }
 }
--- src/main/java/com/example/realtimestatisticsapi/statistics/StatisticsController.java
+++ src/main/java/com/example/realtimestatisticsapi/statistics/StatisticsController.java
@@ -1,8 +1,10 @@
 package com.example.realtimestatisticsapi.statistics;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/statistics")
@@ -12,6 +14,7 @@ public class StatisticsController {
     @Autowired
     public StatisticsService statisticsService;
 
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(method = RequestMethod.GET)
     public Statistics getTransactions() {
         return statisticsService.getAggregatedStatistics();
